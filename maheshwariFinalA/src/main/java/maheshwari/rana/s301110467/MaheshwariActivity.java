6d4f952b3c1c697f1378c312796cf632b0d52186
Maheshwari-Rana <maheshwarivrana@gmail.com>
2020-12-12 08:41:46 -0500

    Initial commit

---
+++ maheshwariFinalA/src/main/java/maheshwari/rana/s301110467/MaheshwariActivity.java
@@ -0,0 +1,14 @@
+package maheshwari.rana.s301110467;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MaheshwariActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
